a1858709aae5f8d7d3466e8737ca9d145d5e4383
Shota Kishimoto <xshotax16@gmail.com>
2022-09-22 11:56:56 +0900

    9/22 ver1.0

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -8,18 +8,16 @@ closetPolyfill();
 // Libraly
 import Loading from "./lib/Loading";
 // import $ from "jquery";
-// import Swiper from "swiper";
+// import Swiper from "swiper/bundle";
 // import SmoothScroll from "smooth-scroll";
-// import ScrollObserver from './lib/ScrollObserver';
+import ScrollObserver from "./lib/ScrollObserver";
 // import Toggle from "./lib/Toggle";
 // import Close from "./lib/Close";
 // import Slider from "./lib/Slider";
 // import { dropdown } from './lib/dropdown';
-// import { inview } from './lib/inview';
-// import Luxy from "luxy";
+import { inview } from "./lib/inview";
 
-// new Swiper();
-// new Luxy();
 new Loading();
 // new SmoothScroll('a[href*="#"]');
 // new Toggle(".js-drawer");
+new ScrollObserver(".c-inview", inview);
